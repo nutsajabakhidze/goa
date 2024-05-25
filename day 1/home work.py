a69030e6c0236c75f24ffd4cb21752bc092e781c
nutsajabakhidze <nutsajabakhidze@gmail.com>
2024-05-25 16:09:52 +0400

    Add files via upload

---
+++ day 1/home work.py
@@ -0,0 +1,34 @@
+name = "nutsa"
+
+print(name)
+
+
+
+
+
+
+
+
+
+
+
+#jf/h
+knows_programing = True
+
+
+
+
+
+
+
+
+
+
+# print(type(age))
+# print(tupe(name))
+# print(tupe(surname))
+# print(tupe(height))
+# print(type(knows_programming))
+
+print(name + " " + str(age))
+
