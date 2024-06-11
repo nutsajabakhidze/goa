2fa439ae84d6d434cec01cafaa5ea7e5169ff96a
nutsajabakhidze <nutsajabakhidze@gmail.com>
2024-06-11 22:38:54 +0400

    Add files via upload

---
+++ day 4/home work.py
@@ -0,0 +1,78 @@
+name = "nutsa"
+last_name = "jabakhidze"
+age = 11
+hobby = "painting"
+country = "Gergia"
+
+
+
+
+
+
+# მიმატება
+
+#print(10 + 10)
+#print(10 + 10) 
+#print(10 + 10)
+#print(10 + 10)
+#print(10 + 10)
+#print(10 + 10)
+#print(10 + 10)
+#print(10 + 10)
+#print(10 + 10)
+#print(10 + 10)
+#print(10 + 10)
+
+# გამოკლება
+
+#print(10 - 10)
+#print(10 - 10)
+#print(10 - 10)
+#print(10 - 10)
+#print(10 - 10)
+#print(10 - 10)
+#print(10 - 10)
+#print(10 - 10)
+#print(10 - 10)
+#print(10 - 10)
+#print(10 - 10)
+
+# გამრავლება
+
+#print(10 * 10) 
+#print(10 * 10)
+#print(10 * 10)
+#print(10 * 10)
+#print(10 * 10)
+#print(10 * 10)
+#print(10 * 10)
+#print(10 * 10)
+#print(10 * 10)
+#print(10 * 10)
+
+# გაყოფა
+
+#print(10 \\ 10)
+#print(10 \\ 10)
+#print(10 \\ 10)
+#print(10 \\ 10)
+#print(10 \\ 10)
+#print(10 \\ 10)
+#print(10 \\ 10)
+#print(10 \\ 10)
+#print(10 \\ 10)
+#print(10 \\ 10)
+
+# დახარისხება
+
+#print(10 ** 10)
+#print(10 ** 10)
+#print(10 ** 10)
+#print(10 ** 10)
+#print(10 ** 10)
+#print(10 ** 10)
+#print(10 ** 10)
+#print(10 ** 10)
+#print(10 ** 10)
+#print(10 ** 10)
+#print(10 ** 10)
---
+++ day5/goa .py
@@ -0,0 +1,30 @@
+  # 1) ცვლადის დასახელება
+  # 2) ტოლობის სიმბოლო
+  # 3) ცვლადის მნიშვნელობა
+
+
+# (1)(2)  (3)
+name = 'nutsa'
+last_name = 'jabakhidze'
+proffesion = 'xatva'
+
+print(name + ' ' + last_name + ' ' + proffesion)
+
+
+# Updating Varieble Value
+
+name = 'nutsa'
+
+name = 'mari'
+
+name = 'lizi'
+
+name = 'nitsa'
+name = 100
+print(name)
+
+
+
+name = 'nutsa'
+last_name = 'jabakhidze'
+print(name)
